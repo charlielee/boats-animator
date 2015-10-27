26a47bad019342f5dfc4f9c7a5997c2692ea14b5
Charlie Lee <charlielee735@gmail.com>
2015-10-27 12:32:17 +0000

    Live preview button now properly scrolls and preview window fills space
    Also made frame reel images slightly bigger but this has resulted in the
    live preview button being slightly off center

--- app/js/main.js
+++ app/js/main.js
@@ -162,6 +162,10 @@ function startup() {
             console.log("height: " + height);
             console.log("width: " + width);
             console.log("Aspect ratio: " + aspectRatio);
+            
+            if (aspectRatio === 1.33) {
+                captureWindow.classList.add("4by3");            
+            }
 
             notifySuccess("Camera successfully connected.");
         }
@@ -288,8 +292,8 @@ function updateFrameReel(action, id) {
 
     // Add the newly captured frame
     if (action === "capture") {
-        frameReelRow.insertAdjacentHTML("beforeend", `<td><div class="frame-reel-preview">
-<img class="frame-reel-img" id="img-${id}" title="Expand image" width="80" height="60" src="${capturedFramesRaw[id - 1]}">
+        frameReelRow.insertAdjacentHTML("afterbegin", `<td><div class="frame-reel-preview">
+<img class="frame-reel-img" id="img-${id}" title="Expand image" width="100" height="75" src="${capturedFramesRaw[id - 1]}">
 <i class="btn-frame-delete fa fa-trash" title="Delete Frame"></i>
 </div></td>`);
 
@@ -479,8 +483,8 @@ function stopitwhenlooping() {
     isPlaying = false;
     //stop increasing playback frame number
     clearInterval(yoplayit);
-    document.getElementById('playback').setAttribute("src", capturedFramesRaw[curFrame - 1]);
-    document.querySelector('#currentFrame span').innerHTML = curFrame;
+    playback.setAttribute("src", capturedFramesRaw[curFrame - 1]);
+    statusBarCurFrame.innerHTML = curFrame;
     btnPlayPause.children[0].classList.remove("fa-pause");
     btnPlayPause.children[0].classList.add("fa-play");
 
