b7524c452c16fd3ac5c879ec08707f2da6347afc
Caleb Ely <le717.code@yahoo.com>
2016-02-20 19:19:22 -0500

    Improve newsfeed
    Make it a little nicer and it now uses window.fetch like I originally
    planned. :smiley:

--- app/js/newsfeed.js
+++ app/js/newsfeed.js
@@ -1,26 +1,39 @@
+  /* global nw */
   module.exports = {};
 
 (function() {
     "use strict";
-    var newsContainer = window.document.querySelector(".container-news");
+    let qNewsContainer = document.querySelector(".container-news");
+    qNewsContainer.addEventListener("click", openNews);
+
+    /**
+     * Open the desired news post in the user's browser.
+     *
+     * @param {Object} e - An MouseEvent object.
+     */
+    function openNews(e) {
+        if (e.target.classList.contains("post-title")) {
+            nw.Shell.openExternal(e.target.parentElement.dataset.url);
+        }
+    }
 
     /**
      * Cache the latest news.
      *
-     * @param {Array} posts An array containing raw new objects.
+     * @param {Array} posts - An array containing raw new objects.
      * @returns {Boolean} True if new was cached, false otherwise.
      */
-    function cacheNews(posts) {
+    function _cacheNews(posts) {
         // Store the news in localStorage in case we cannot fetch it anew
-        window.localStorage.setItem("ba-news", JSON.stringify(posts));
+        localStorage.setItem("ba-news", JSON.stringify(posts));
 
         // Because setItem() does not have a return value upon successful
         // storage, this kludge does that for us.
-        if (window.localStorage.getItem("ba-news")) {
-            console.info("Latest news sucessfully cached.");
+        if (localStorage.getItem("ba-news")) {
+            console.info("Latest news sucessfully cached");
             return true;
         } else {
-            console.info("Latest news sucessfully cached.");
+            console.info("Latest news was not sucessfully cached");
             return false;
         }
     }
@@ -31,8 +44,9 @@
      * @returns {Array} An array containing raw news objects.
      *                  If not available, an empty array.
      */
-    function getNewsCache() {
-        var cached = window.localStorage.getItem("ba-news");
+    function _getNewsCache() {
+        console.info("Fetching cached news");
+        var cached = localStorage.getItem("ba-news");
         return cached ? JSON.parse(cached) : [];
     }
 
@@ -41,21 +55,21 @@
      *
      * @returns {Boolean} Always returns true.
      */
-    function clearNewsCache() {
-        window.localStorage.removeItem("ba-news");
+    function _clearNewsCache() {
+        localStorage.removeItem("ba-news");
         return true;
     }
 
     /**
      * Construct the HTML structure for the news.
      *
-     * @param {Object} post A news article object.
+     * @param {Object} post - A news article object.
      * @returns {String}
      */
-    function compilePostHTML(post) {
+    function _compilePostHTML(post) {
         return `<div class="post" id="post-${post.id}">
     <header>
-        <a class="post-link" href="#" onclick="gui.Shell.openExternal('${post.url}')"><h2 class="post-title">${post.title}</h2></a>
+        <a class="post-link" href="#" data-url="${post.url}"><h2 class="post-title">${post.title}</h2></a>
         <p class="post-date">${post.date}</p>
     </header>
     <div class="post-content"><p>${post.excerpt}</p></div>
@@ -65,13 +79,12 @@
     /**
      * Compile the latest news.
      *
-     * @param {JSON} data The raw JSON response data.
+     * @param {JSON} data - The JSON response data.
      * @returns {Array} The latest news, in the form of news objects.
      */
     function getPosts(data) {
         var posts      = [],
             numOfPosts = 3;
-        data = JSON.parse(data);
 
         // Get the post up to the limit of posts we want to display
         for (var i = 0; i < numOfPosts; i++) {
@@ -86,8 +99,8 @@
         }
 
         // Cache the news
-        clearNewsCache();
-        cacheNews(posts);
+        _clearNewsCache();
+        _cacheNews(posts);
         return posts;
     }
 
@@ -95,19 +108,19 @@
      * Display an error message if the news cannot be loaded.
      */
     function erroring(err) {
-        newsContainer.innerHTML = "<h3>News could not be loaded at this time.</h3>";
+        qNewsContainer.innerHTML = "<h3>News could not be loaded at this time.</h3>";
         console.error(err);
     }
 
     /**
      * Display the news.
      *
-     * @param {Array} posts An array containing the news to display.
+     * @param {Array} posts - An array containing the news to display.
      * @returns {Boolean} Always returns true.
      */
     function displayNews(posts) {
         posts.forEach(function(post) {
-            newsContainer.insertAdjacentHTML("beforeend", compilePostHTML(post));
+            qNewsContainer.insertAdjacentHTML("beforeend", _compilePostHTML(post));
         });
         return true;
     }
@@ -115,36 +128,23 @@
     /**
      * Fetch the latest news.
      *
-     * @param {String} url The URL to the JSON news feed.
+     * @param {String} url - The URL to the JSON news feed.
      */
     function load(url) {
-        // We can fetch the news from the site
-        var request = new window.XMLHttpRequest();
-        request.open("GET", url, true);
-        request.send();
+        window.fetch(url, {method: "get"})
+        .then(r => r.json())
+        .then(r => displayNews(getPosts(r)))
+        .catch(err => {
+            // First try to load the cached news, if possible
+            var cachedNews = _getNewsCache();
+            if (cachedNews) {
+                displayNews(cachedNews);
 
-        // Success!
-        request.onload = function() {
-            if (request.readyState === 4 && request.status === 200) {
-                // Extract and display the latest news
-                displayNews(getPosts(request.responseText));
-
-            // We reached our target destination, but it returned an error
+                // No cache available, error
             } else {
-                // First try to load the cached news, if possible
-                var cachedNews = getNewsCache();
-                if (cachedNews) {
-                    displayNews(cachedNews);
-
-                    // No cache available, error
-                } else {
-                    erroring();
-                }
+                erroring(err);
             }
-        };
-
-        // We could not fetch the feed
-        request.onerror = erroring;
+        });
     }
 
     // Public exports
