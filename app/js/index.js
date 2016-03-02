bc7985381465f7c433114a86a7cee715a34f89e3
le717.legend <le717.legend@outlook.com>
2016-03-02 11:00:43 -0500

    Finished encapsulating index JS

--- app/js/index.js
+++ app/js/index.js
@@ -1,46 +1,46 @@
 /*jslint browser: true, node: true, debug: true*/
 /* global Buffer, process */
 
-// Launcher window
-var launcherVersion = document.querySelector("#app-version"),
-
-    // GUI window
-    gui = require("nw.gui"),
-    win = gui.Window.get(),
-
-    // Node modules
-    file     = require("./js/file"),
-    newsFeed = require("./js/newsfeed");
-
-/**
- * Occurs when "New Project" is pressed
- */
-function openAnimator() {
+(function() {
     "use strict";
-    gui.Window.open("animator.html", {
-        position: "center",
-        width: 1050,
-        height: 715,
-        min_width: 590,
-        min_height: 500,
-        toolbar: false,
-        focus: true,
-        icon: "icons/icon.png",
-    });
-    win.close();
-}
+    // Launcher window
+    var launcherVersion = document.querySelector("#app-version"),
 
-/**
- * Load the news feed.
- */
-function loadNews() {
-    "use strict";
+        // GUI window
+        gui = require("nw.gui"),
+        win = gui.Window.get(),
+
+        // Node modules
+        file     = require("./js/file"),
+        newsFeed = require("./js/newsfeed");
+
+    /**
+     * Occurs when "New Project" is pressed
+     */
+    function openAnimator() {
+        gui.Window.open("animator.html", {
+            position: "center",
+            width: 1050,
+            height: 715,
+            min_width: 590,
+            min_height: 500,
+            toolbar: false,
+            focus: true,
+            icon: "icons/icon.png",
+        });
+        win.close();
+    }
+
+    // Get the version number from the manifest file
+    launcherVersion.innerHTML = gui.App.manifest.version;
+
+    // Load the news feed
     newsFeed.load("http://charlielee.uk/api/core/get_category_posts/?id=12");
-}
 
-// Get the version number from the manifest file
-launcherVersion.innerHTML = gui.App.manifest.version;
+    // Start a new project
+    document.querySelector("#new-project").addEventListener("click", openAnimator);
 
-// Developer buttons
-document.querySelector("#btn-open-dev-tools").addEventListener("click", utils.showDev);
-document.querySelector("#btn-reload-page").addEventListener("click", utils.reloadPage);
+    // Developer buttons
+    document.querySelector("#btn-open-dev-tools").addEventListener("click", utils.showDev);
+    document.querySelector("#btn-reload-page").addEventListener("click", utils.reloadPage);
+}());
