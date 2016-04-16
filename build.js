a4d96b49de3719449dfe90ff201a7ec47c91901d
Charlie Lee <charlielee735@gmail.com>
2016-04-16 12:26:06 +0100

    Build method fixes

--- build.js
+++ build.js
@@ -1,15 +1,15 @@
 var userDir = process.env.USERPROFILE,
-    NwBuilder = require(userDir + "\\AppData\\Roaming\\npm\\node_modules\\nw-builder"),
+    NwBuilder = require(`${userDir}\\AppData\\Roaming\\npm\\node_modules\\nw-builder`),
     nw = new NwBuilder({
       files: "app\\**\\**", // use the glob format
-      platforms: ["linux64", "linux32", "osx64", "win32", "win64"],
+      platforms: ["linux64", "linux32", "osx64", "win32"],
       version: "0.12.3",
       appName: "Boats Animator",
       buildDir: "bin",
-      cacheDir: userDir + "\\AppData\\Roaming\\npm\\node_modules\\nw-builder\\cache",
+      cacheDir: `${userDir}\\AppData\\Roaming\\npm\\node_modules\\nw-builder\\cache`
     });
 
-nw.on("log",  console.log);
+nw.on("log", console.log);
 
 // Build returns a promise
 nw.build().then(function () {
