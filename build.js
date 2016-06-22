da70b286ee4064420705dc244caeb23dc304c91d
Charlie Lee <charlielee735@gmail.com>
2016-06-22 21:33:46 +0100

    Fix #132 and #134

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
-var nwjsBuilder = require("nwjs-builder"),
+var fs = require("fs.extra"),
+    nwjsBuilder = require("nwjs-builder"),
     options = {
       platforms: "linux64,linux32,osx64,win32",
       version: "0.14.5",
@@ -6,12 +7,42 @@ var nwjsBuilder = require("nwjs-builder"),
       outputName: "Boats-Animator-{version}-{target}",
       outputFormat: "DIR",
       executableName: "BoatsAnimator",
-      winIco: "win-install/icon.ico",
-      macIcns: "app/icons/icon.icns"
+      winIco: "icons/icon.ico",
+      macIcns: "icons/icon.icns"
     };
 
-// Build returns a promise
-nwjsBuilder.commands.nwbuild("app", options, function(err) {
-  if (err) console.log(err);
-  console.log("Done!");
+// Move the files to be packaged to a temp directory.
+fs.mkdir("temp", function(err) {
+  console.log(err ? err : "Create temp directory");
+
+  fs.copy("package.json", "temp/package.json", function(err) {
+    console.log(err ? err : "Copy package.json");
+
+    fs.copyRecursive("icons", "temp/icons", function(err) {
+      fs.delete("temp/icons/icon.icns", function(err) {
+        if (err) console.log(err);
+      });
+      fs.delete("temp/icons/icon.ico", function(err) {
+        if (err) console.log(err);
+      });
+      console.log(err ? err : "Copy icon.png");
+
+      fs.copyRecursive("app", "temp/app", function(err) {
+        console.log(err ? err : "Copy app directory");
+        build();
+      });
+    });
+  });
 });
+
+// Use nwjs-builder to create the output packages.
+function build() {
+  nwjsBuilder.commands.nwbuild("temp", options, function(err) {
+    console.log(err ? err : "Finished exporting packages");
+
+    fs.rmrf("temp", function(err) {
+      console.log(err ? err : "Delete temp directory");
+      console.log("Done!");
+    });
+  });
+}
