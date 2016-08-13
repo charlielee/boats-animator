f910dcc5633793d8ccfdac82fe5ad9f5cfbf6b44
Caleb Ely <le717.code@yahoo.com>
2016-08-12 20:13:32 -0400

    Detect connected cameras

--- app/js/camera.js
+++ app/js/camera.js
@@ -18,5 +18,36 @@ module.exports = {};
         }
       };
 
+
+  // Get the DOM selectors needed
+  let qResoluSelect = document.querySelector("#form-resolution-select"),
+      qCameraSelect = document.querySelector("#camera-select-td select"),
+      videoCapture  = document.createElement("video");
+
+    function _findVideoSources(sources) {
+      let i = 1;
+      sources.forEach(function(source) {
+        if (source.kind === "video") {
+          // Get the proper camera name
+          let cameraName = `Camera ${i}`;
+          if (source.label) {
+            cameraName = source.label.substr(0, source.label.indexOf("(") - 1);
+          }
+
+          // Create the menu selection
+          var option = window.document.createElement("option");
+          option.text = cameraName;
+          option.value = source.id;
+          qCameraSelect.appendChild(option);
+          i++;
+        }
+      });
+
+      // Default select the first camera
+      qCameraSelect.options[0].selected = true;
+    }
+
+    // Get the available cameras
+    window.MediaStreamTrack.getSources(_findVideoSources);
   // Public exports
 }());
