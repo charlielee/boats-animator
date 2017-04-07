bfbcd1385f0aa111c2dcddcb586b35f4c15a2df4
Charlie Lee <dev@charlielee.uk>
2017-04-07 15:56:24 +0100

    Add live reloading function to utils.js

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,5 +1,13 @@
 const utils = (function() {
   "use strict";
+  let devLiveReloadScript = document.querySelector("#dev-reload-script"),
+      liveReload          = false;
+
+  // Get live reload setting from current session
+  if (sessionStorage.getItem("ba-dev-live-reload")) {
+    liveReload = (sessionStorage.getItem("ba-dev-live-reload") === "true");
+    setLiveReload(liveReload);
+  }
 
   /**
    * Open a URL in the user's browser.
@@ -10,7 +18,21 @@ const utils = (function() {
     nw.Shell.openExternal(url);
   }
 
+  /**
+   * Sets whether to live reload or not (for development purposes).
+   * 
+   * @param {boolean} reload - Set to true to enable live reloading.
+   */
+  function setLiveReload(reload) {
+    // Update script tag src
+    devLiveReloadScript.src = (reload ? "../node_modules/nw-dev/lib/dev.js" : "");
+
+    // Update session storage item
+    sessionStorage.setItem("ba-dev-live-reload", reload);
+  }
+
   return {
-    openURL: openURL
+    openURL: openURL,
+    setLiveReload: setLiveReload
   };
 }());
