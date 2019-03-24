bdfc82f436ccb7097f691015df4d3e977af123f4
Charlie Lee <dev@charlielee.uk>
2019-03-24 23:54:27 +0000

    Start frame reel refactor

---
+++ app/ui/FrameReel/FrameReel.js
@@ -0,0 +1,31 @@
+var frameReelArea = document.querySelector("#area-frame-reel");
+var frameReelMsg = document.querySelector("#area-frame-reel > p");
+var frameReelRow = document.querySelector("#area-frame-reel #reel-captured-imgs");
+var frameReelTable = document.querySelector("#area-frame-reel table");
+
+var btnLiveView = document.querySelector("#btn-live-view");
+var liveViewframeNo = document.querySelector("#live-view-frame-no");
+
+// todo
+class FrameReel {
+  constructor() {
+  }
+
+  addFrame(id) {
+
+  }
+
+  removeFrame(id) {
+
+  }
+
+  selectFrame(id) {
+
+  }
+
+  selectLiveViewButton() {
+
+  }
+}
+
+module.exports = FrameReel;
