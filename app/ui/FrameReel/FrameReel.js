222c7491800b7be691b1f439d0d66b65d05c3b9a
Charlie Lee <dev@charlielee.uk>
2019-03-25 00:16:20 +0000

    More work on frame reel refactor

--- app/ui/FrameReel/FrameReel.js
+++ app/ui/FrameReel/FrameReel.js
@@ -9,23 +9,51 @@ var liveViewframeNo = document.querySelector("#live-view-frame-no");
 // todo
 class FrameReel {
   constructor() {
+    // The id of the currently selected frame
+    this.curSelectedFrame = null;
+    // Whether the live view button is selected or not
+    this.liveViewButtonSelected = false;
   }
 
+  /**
+   * Adds a frame to the frame reel.
+   * @param {integer} id The id of the frame to add (note ids should start at 1).
+   */
   addFrame(id) {
 
   }
 
+  /**
+   * Removes a frame from the frame reel.
+   * @param {integer} id The id of the frame to remove (note ids should start at 1).
+   */
   removeFrame(id) {
 
   }
 
+  /**
+   * Highlights and scrolls to a given frame on the frame reel.
+   * @param {integer} id The id of the frame to highlight and scroll to.
+   */
   selectFrame(id) {
 
   }
-
-  selectLiveViewButton() {
-
+  
+  /**
+   * Chooses whether an outline should be displayed around the live view button.
+   * @param {boolean} select Set to true to select the live view button,
+   *                         false to deselect it.
+   */
+  selectLiveViewButton(select) {
+    this.liveViewButtonSelected = select;
+    if (select) {
+      btnLiveView.classList.add("selected");
+    } else {
+      btnLiveView.classList.remove("selected");
+    }
   }
+
+
 }
 
 module.exports = FrameReel;
