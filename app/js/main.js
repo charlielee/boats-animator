da925dd9759b5be3f87d058847eeba93f29c2a83
Charlie Lee <dev@charlielee.uk>
2019-04-11 00:21:01 -0400

    Create Take module

--- app/js/main.js
+++ app/js/main.js
@@ -8,20 +8,21 @@ var streaming = false,
   // NW.js
   win = nw.Window.get();
 
+  // Main imports
+  var Take = require("./main/Take/Take");
+
+  // Main instances
+  var takeInst = new Take(1);
+
   // UI imports
   var FrameReel = require("./ui/FrameReel/FrameReel");
   var Notification = require("./ui/Notification/Notification");
-  var OnionSkin = require("./ui/OnionSkin/OnionSkin");
   var PlaybackCanvas = require("./ui/PlaybackCanvas/PlaybackCanvas");
   var PreviewArea = require("./ui/PreviewArea/PreviewArea");
   var StatusBar = require("./ui/StatusBar/StatusBar");
 
   // UI instances
   var frameReelInst = new FrameReel();
-  var onionSkinInst = new OnionSkin();
-
-  // Common imports
-  var AudioManager = require("./common/AudioManager/AudioManager");
 
   // Mode switching
   var btnLiveView = document.querySelector("#btn-live-view"),
@@ -32,8 +33,6 @@ var streaming = false,
   Camera = require("./js/camera"),
 
   // Capture
-  capturedFrames = [],
-  totalFrames = 0,
   btnCaptureFrame = document.querySelector("#btn-capture-frame"),
   btnDeleteLastFrame = document.querySelector("#btn-delete-last-frame"),
 
@@ -57,7 +56,6 @@ var streaming = false,
   resolutionSelect = document.querySelector("#resolution-select-td select"),
 
   // Frame export
-  exportedFramesList = [],
   curDirDisplay = document.querySelector("#currentDirectoryName"),
 
   // Frame reel
@@ -236,7 +234,7 @@ function startup() {
 
   // Play/pause the preview
   btnPlayPause.addEventListener("click", function () {
-    if (totalFrames > 0) {
+    if (takeInst.getTotalFrames() > 0) {
       (isPlaying ? videoPause : previewCapturedFrames)();
     }
   });
@@ -251,7 +249,7 @@ function startup() {
   // Preview one frame to the right on framereel
   btnFrameNext.addEventListener("click", function () {
     if (frameReelInst.curSelectedFrame) {
-      if (frameReelInst.curSelectedFrame !== totalFrames) {
+      if (frameReelInst.curSelectedFrame !== takeInst.getTotalFrames()) {
         _displayFrame(frameReelInst.curSelectedFrame + 1);
       } else {
         switchMode(CaptureWindow);
@@ -263,15 +261,15 @@ function startup() {
   btnFramePrevious.addEventListener("click", function () {
     if (frameReelInst.curSelectedFrame > 1) {
       _displayFrame(frameReelInst.curSelectedFrame - 1);
-    } else if (PreviewArea.curWindow === CaptureWindow && totalFrames) {
+    } else if (PreviewArea.curWindow === CaptureWindow && takeInst.getTotalFrames()) {
       switchMode(PlaybackWindow);
-      _displayFrame(totalFrames);
+      _displayFrame(takeInst.getTotalFrames());
     }
   });
 
   // Preview first frame on framereel
   btnFrameFirst.addEventListener("click", function () {
-    if (PreviewArea.curWindow === CaptureWindow && totalFrames) {
+    if (PreviewArea.curWindow === CaptureWindow && takeInst.getTotalFrames()) {
       switchMode(PlaybackWindow);
     }
     _displayFrame(1);
@@ -280,7 +278,7 @@ function startup() {
   // Preview last frame on framereel
   btnFrameLast.addEventListener("click", function () {
     if (frameReelInst.curSelectedFrame) {
-      (frameReelInst.curSelectedFrame !== totalFrames ? videoStop : switchMode(CaptureWindow))();
+      (frameReelInst.curSelectedFrame !== takeInst.getTotalFrames() ? videoStop : switchMode(CaptureWindow))();
     }
   });
 
@@ -342,7 +340,7 @@ function switchMode(NewWindow) {
 
   if (PreviewArea.curWindow === CaptureWindow) {
     videoStop();
-    StatusBar.setCurrentFrame(totalFrames + 1);
+    StatusBar.setCurrentFrame(takeInst.getTotalFrames() + 1);
     StatusBar.setMode("Capture");
     frameReelInst.selectLiveViewButton();
 
@@ -355,65 +353,15 @@ function switchMode(NewWindow) {
 }
 
 /**
- * Update the frame reel display as needeed.
- *
- * @param {String} action Update the frame reel depending on the
- *                        action performed. Possible values are
- *                        "capture" and "delete".
- * @param {Number} id The image ID to use in the update.
- */
-function updateFrameReel(action, id) {
-  "use strict";
-  var onionSkinFrame = id - 1;
-  // Display number of captured frames in status bar
-  StatusBar.setTotalFrames(totalFrames);
-
-  // Add the newly captured frame
-  if (action === "capture") {
-    frameReelInst.addFrame(id, capturedFrames[id - 1].src);
-    frameReelInst.setFrameThumbnail(id, capturedFrames[id - 1].src)
-
-    // Remove the chosen frame
-  } else if (action === "delete") {
-    if (id !== totalFrames) {
-      onionSkinFrame = id - 2;
-    }
-    frameReelInst.removeFrame(id)
-  }
-
-  // Switch to capture mode
-  // todo in the future "delete any frame" may mean sometimes
-  // we should switch to playback mode
-  switchMode(CaptureWindow);
-
-  // We have frames, display them
-  if (totalFrames > 0) {
-    // Update onion skin frame
-    onionSkinInst.setFrame(capturedFrames[onionSkinFrame].src);
-  } else {
-    // Clear the onion skin window
-    onionSkinInst.clear();
-  }
-}
-
-/**
  * Delete an individual frame.
  *
  * @param {Number} id The frame ID to delete.
  */
 function deleteFrame(id) {
   "use strict";
-  file.delete(exportedFramesList[id - 1], {
-    success: function () {
-      Notification.success("File successfully deleted.");
-    }
-  });
-
-  exportedFramesList.splice(id - 1, 1);
-  capturedFrames.splice(id - 1, 1);
-  totalFrames--;
-  updateFrameReel("delete", id);
-  console.info(`Total frames captured: ${totalFrames}`);
+  takeInst.deleteFrame(id);
+  switchMode(CaptureWindow);
+  console.info(`Total frames captured: ${takeInst.getTotalFrames()}`);
 }
 
 /**
@@ -440,12 +388,12 @@ function takeFrame() {
 function undoFrame() {
   "use strict";
   // Make sure there is a frame to delete
-  if (totalFrames > 0) {
+  if (takeInst.getTotalFrames() > 0) {
     // Display warning alert to confirm deletion
     confirmSet({text: "Are you sure you want to delete the last frame captured?"})
     .then((response) => {
       if (response) {
-        deleteFrame(totalFrames);
+        deleteFrame(takeInst.getTotalFrames());
       }
     });
   } else {
@@ -459,34 +407,11 @@ function _captureFrame() {
   // Stop playback
   videoStop();
 
-  if (PreviewArea.curWindow === PlaybackWindow) {
-    switchMode(CaptureWindow);
-  }
+  switchMode(CaptureWindow);
 
   // Take a picture
   if (streaming) {
-    // Draw the image on the canvas
-    PlaybackCanvas.setDimensions(preview.videoWidth, preview.videoHeight);
-    PlaybackCanvas.drawImage(preview);
-
-    // Convert the frame to a PNG
-    PlaybackCanvas.getBlob(function(blob) {
-      // Play a camera sound
-      AudioManager.play("audio/camera.wav");
-
-      var frame = new Image();
-      var url = URL.createObjectURL(blob);
-      frame.src = url;
-
-      // Store the image data and update the current frame
-      capturedFrames.push(frame);
-      totalFrames++;
-      console.info(`Captured frame: ${totalFrames}`);
-
-      // Save the frame to disk and update the frame reel
-      updateFrameReel("capture", totalFrames);
-      saveFrame(totalFrames, blob);
-    });
+    takeInst.captureFrame();
   }
 }
 
@@ -534,8 +459,8 @@ function videoStop() {
   "use strict";
   videoPause();
   curPlayFrame = 0;
-  if (totalFrames > 0 && PreviewArea.curWindow === PlaybackWindow) {
-    _displayFrame(totalFrames);
+  if (takeInst.getTotalFrames() > 0 && PreviewArea.curWindow === PlaybackWindow) {
+    _displayFrame(takeInst.getTotalFrames());
   }
   console.info("Playback stopped");
 }
@@ -547,13 +472,13 @@ function videoStop() {
  */
 function _displayFrame(id) {
   "use strict";
-  if (totalFrames > 0) {
+  if (takeInst.getTotalFrames() > 0) {
     // Reset the player
     videoPause();
 
     // Preview selected frame ID
     frameReelInst.selectFrame(id);
-    PlaybackCanvas.drawImage(capturedFrames[id - 1]);
+    PlaybackCanvas.drawImage(takeInst.capturedFrames[id - 1]);
     StatusBar.setCurrentFrame(id);
   }
 
@@ -570,7 +495,7 @@ function _videoPlay() {
     playBackRAF = requestAnimationFrame(_videoPlay);
 
     // There are no more frames to preview
-    if (curPlayFrame >= totalFrames) {
+    if (curPlayFrame >= takeInst.getTotalFrames()) {
       // We are not looping, stop the playback
       if (!isLooping) {
         switchMode(CaptureWindow);
@@ -583,7 +508,7 @@ function _videoPlay() {
     }
 
     // Display each frame and update the UI accordingly
-    PlaybackCanvas.drawImage(capturedFrames[curPlayFrame]);
+    PlaybackCanvas.drawImage(takeInst.capturedFrames[curPlayFrame]);
     StatusBar.setCurrentFrame(curPlayFrame + 1);
     frameReelInst.selectFrame(curPlayFrame + 1);
 
@@ -604,7 +529,7 @@ function previewCapturedFrames() {
 
   // Reset canvas to first frame if playing from start
   if (curPlayFrame == 0) {
-    PlaybackCanvas.drawImage(capturedFrames[0]);
+    PlaybackCanvas.drawImage(takeInst.capturedFrames[0]);
   }
 
   // Update the play/pause button
@@ -636,51 +561,6 @@ function _displaySaveDirectory(dir) {
   curDirDisplay.innerHTML = dir;
   document.title = `Boats Animator (${dir})`;
   Notification.success(`Current save directory is ${dir}`);
-}
-
-/**
- * Save the captured frame to the hard drive.
- *
- * @param {Number} id - The frame ID to save.
- * @param {Blob} blob - The Blob object containing image data to save.
- */
-function saveFrame(id, blob) {
-  "use strict";
-  var fileName = "";
-
-  // 1K+ frames have been captured
-  if (id >= 1000) {
-    fileName = `frame_${id}`;
-  }
-
-  // 100 frames have been captured
-  else if (id >= 100) {
-    fileName = `frame_0${id}`;
-  }
-
-  // 10 frames have been captured
-  else if (id >= 10) {
-    fileName = `frame_00${id}`;
-
-    // Less then 10 frames have been captured
-  } else {
-    fileName = `frame_000${id}`;
-  }
-
-  // Create an absolute path to the destination location
-  var outputPath = `${saveDirectory.get()}/${fileName}.png`;
-
-  // Save the frame to disk
-  var reader = new FileReader()
-  reader.onload = function(){
-    // Convert the frame blob to buffer
-    var buffer = new Buffer.from(reader.result)
-    file.write(outputPath, buffer);
-  }
-  reader.readAsArrayBuffer(blob)
-
-  // Store the location of the exported frame
-  exportedFramesList.push(outputPath);
 }
 
 /**
