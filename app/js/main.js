eaa1d451712f5b3ac0233b6fdf99f90c73a16d44
Charlie Lee <dev@charlielee.uk>
2019-06-11 14:20:29 +0100

    Make dynamic grid actually display

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 (function() {
   "use strict";
   // Main imports
+  const PreviewOverlay = require("./main/PreviewOverlay/PreviewOverlay");
   const Project = require("./main/Project/Project");
   global.AppShortcuts = require("./main/Shortcuts/Shortcuts");
 
@@ -24,6 +25,7 @@
       // UI initialisation
       CaptureOptions.setListeners();
       FrameReelRow.setListeners();
+      PreviewOverlay.initialise();
       WindowManager.setListeners();
       global.projectInst.setListeners();
 
