b2fa8da2d52af0d6d0341025307eb7664eb2e282
Charlie Lee <dev@charlielee.uk>
2020-09-22 20:05:05 +0100

    Remove nw-dev as it hasn't been updated in 4 years and no longer works

--- app/common/Utils/Utils.js
+++ app/common/Utils/Utils.js
@@ -1,12 +1,6 @@
 const utils = (function() {
   "use strict";
 
-  // Get live reload setting from current session
-  let liveReload = sessionStorage.getItem("ba-dev-live-reload");
-  if (liveReload) {
-    setLiveReload(liveReload === "true");
-  }
-
   /**
    * Open a URL in the user's browser.
    *
@@ -17,20 +11,7 @@ const utils = (function() {
     nw.Shell.openExternal(url);
   }
 
-  /**
-   * Enable live code reloading for development purposes.
-   *
-   * @param {Boolean} [reload=true] - Set to false to disable.
-   * @returns {void}
-   */
-  function setLiveReload(reload=true) {
-    document.querySelector("#dev-reload-script").src = (
-      reload ? "../node_modules/nw-dev/lib/dev.js" : "");
-    sessionStorage.setItem("ba-dev-live-reload", `${reload}`);
-  }
-
   return {
-    openURL: openURL,
-    setLiveReload: setLiveReload
+    openURL: openURL
   };
 }());
