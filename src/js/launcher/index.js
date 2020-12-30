d477a9e5f93610bc9edda762fbbe3a7be399eb13
Charlie Lee <dev@charlielee.uk>
2020-12-30 17:47:05 +0000

    Warn macos developers not to use VS code

--- src/js/launcher/index.js
+++ src/js/launcher/index.js
@@ -1,4 +1,4 @@
-(async function() {
+(function() {
   "use strict";
   const { ipcRenderer } = require('electron');
 
@@ -6,11 +6,14 @@
   const Notification = require("./js/common/Notification");
 
   const newProjectBtn = document.querySelector("#new-project");
+  const launcherMessage = document.querySelector("#launcher-message");
   const loadProjectBtn = document.querySelector("#load-project");
   const qAppVersion = document.querySelector("#app-version");
 
   // Get the version number from the manifest file
   qAppVersion.innerHTML = require('../package').version;
+
+  checkDevelopmentMode();
 
   // Display the latest news
   newsFeed.load("http://charlielee.uk/feed/boats-animator.json");
@@ -25,10 +28,36 @@
     Notification.info("This feature is not yet available!");
   });
 
-  // Display development mode notification
-  let isPackaged = await ipcRenderer.invoke('app:is-packaged');
-  
-  if (!isPackaged) {
-    Notification.info("Boats Animator has been started in development mode.")
+  /**
+   * Displays a message if the app is in development mode
+   */
+  async function checkDevelopmentMode() {
+    let isPackaged = await ipcRenderer.invoke('app:is-packaged');
+
+    if (!isPackaged) {
+      // On macOS display additional warning about VS Code not working
+      if (process.platform === "darwin") {
+        launcherMessage.innerHTML = `
+          <div class="highlight-body">
+            <h2>
+              <i class="fa fa-user-secret" aria-hidden="true"></i>
+              App started in development mode on macOS
+            </h2>
+
+            Please ensure that you start this app from the built in Terminal application rather than Visual Studio Code or a similar integration.
+            These will cause the app to crash due to the camera permission being unable to be set.
+          </div>
+        `;
+      } else {
+        launcherMessage.innerHTML = `
+          <div class="highlight-body">
+            <h2>
+              <i class="fa fa-user-secret" aria-hidden="true"></i>
+              App started in development mode
+            </h2>
+          </div>
+        `;
+      }
+    }
   }
 }());
