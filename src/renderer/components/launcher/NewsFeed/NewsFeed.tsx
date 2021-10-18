345b0b618e47e226485588e52e0ebc40320a2313
Charlie Lee <dev@charlielee.uk>
2021-10-18 14:01:11 +0100

    Handle if error fetching news

--- src/renderer/components/launcher/NewsFeed/NewsFeed.tsx
+++ src/renderer/components/launcher/NewsFeed/NewsFeed.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchRecent } from "../../../services/news/News";
+import NewsDownloadError from "../../../services/news/NewsDownloadError";
 import { NewsResponsePost } from "../../../services/news/NewsResponse";
 import "./NewsFeed.css";
 
@@ -7,6 +8,7 @@ interface NewsFeedProps {}
 
 const NewsFeed = (): JSX.Element => {
   const [newPosts, setNewsPosts] = useState<NewsResponsePost[]>([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -14,27 +16,38 @@ const NewsFeed = (): JSX.Element => {
         const newsResponse = await fetchRecent();
         setNewsPosts(newsResponse.posts);
       } catch (e) {
-        console.error(e);
+        if (e instanceof NewsDownloadError) {
+          setError(true);
+        } else {
+          console.error(e);
+        }
       }
     })();
   }, []);
 
   return (
     <div className="news-feed">
-      {newPosts.map((post) => (
-        <div key={post.id}>
-          <h3>
-            <a
-              href="#"
-              onClick={() => window.preload.openExternal.newsPost(post)}
-            >
-              {post.title}
-            </a>
-          </h3>
-          <p>{post.date}</p>
-          <div dangerouslySetInnerHTML={{ __html: post.excerpt }}></div>
-        </div>
-      ))}
+      {error ? (
+        <p className="news-feed__error">
+          News could not be loaded at this time.
+        </p>
+      ) : (
+        newPosts.map((post) => (
+          <div key={post.id}>
+            <h3>
+              <a
+                href="#"
+                onClick={() => window.preload.openExternal.newsPost(post)}
+              >
+                {post.title}
+              </a>
+            </h3>
+            <p>{post.date}</p>
+
+            <div dangerouslySetInnerHTML={{ __html: post.excerpt }}></div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
