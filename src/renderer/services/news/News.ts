4e91fe3cbaa6777d15103fe20db20f37bd9aa4fb
Charlie Lee <dev@charlielee.uk>
2021-10-18 13:41:04 +0100

    Limit number of posts in recent news

--- src/renderer/services/news/News.ts
+++ src/renderer/services/news/News.ts
@@ -2,14 +2,17 @@ import NewsDownloadError from "./NewsDownloadError";
 import { NewsResponse } from "./NewsResponse";
 
 const NEWS_URL = "https://www.charlielee.uk/feed/boats-animator.json";
+const POST_COUNT = 5;
 
 export const fetchRecent = async (): Promise<NewsResponse> => {
   try {
-    return window
+    const newsResponse = await window
       .fetch(NEWS_URL, {
         method: "get",
       })
       .then((data) => data.json());
+
+    return { ...newsResponse, posts: newsResponse.posts.slice(0, POST_COUNT) };
   } catch (e) {
     throw new NewsDownloadError();
   }
