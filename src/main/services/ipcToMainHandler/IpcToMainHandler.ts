41ee1f86b18352d045177139e18411acf9f33324
Charlie Lee <charlie.lee@glean.co>
2021-11-26 11:11:44 +0000

    Refactor AppWindow not to inherit from BrowserWindow

--- src/main/services/ipcToMainHandler/IpcToMainHandler.ts
+++ src/main/services/ipcToMainHandler/IpcToMainHandler.ts
@@ -1,27 +1,30 @@
 import { app, BrowserWindow, ipcMain, IpcMainInvokeEvent } from "electron";
 import IpcChannel from "../../../common/ipc/IpcChannel";
 import Ipc from "../../../common/ipc/IpcHandler";
-import AppWindow from "../appWindow/AppWindow";
-import SettingsFileStore from "../fileStore/SettingsFileStore";
+import {
+  getWindowSize,
+  openConfirmPrompt,
+  openDirDialog,
+} from "../windowUtils.ts/windowUtils";
+import { settingsFileStore } from "../fileStore/SettingsFileStore";
 
 class IpcToMainHandler {
-  constructor(private settingsFileStore: SettingsFileStore) {}
+  constructor() {}
 
   appVersion = async (e: IpcMainInvokeEvent): Ipc.AppVersion.Response =>
     app.getVersion();
 
   getUserPreferences = async (
     e: IpcMainInvokeEvent
-  ): Ipc.GetUserPreferences.Response =>
-    this.settingsFileStore.get().userPreferences;
+  ): Ipc.GetUserPreferences.Response => settingsFileStore.get().userPreferences;
 
   saveSettingsAndClose = async (
     e: IpcMainInvokeEvent,
-    win: AppWindow,
+    win: BrowserWindow,
     payload: Ipc.SaveSettingsAndClose.Payload
   ): Ipc.SaveSettingsAndClose.Response => {
-    this.settingsFileStore.save({
-      appWindowSize: win.getWindowSize(),
+    settingsFileStore.save({
+      appWindowSize: getWindowSize(win),
       userPreferences: payload.userPreferences,
     });
     win.destroy();
@@ -29,30 +32,34 @@ class IpcToMainHandler {
 
   openConfirmPrompt = (
     e: IpcMainInvokeEvent,
-    win: AppWindow,
+    win: BrowserWindow,
     payload: Ipc.OpenConfirmPrompt.Payload
-  ): Ipc.OpenConfirmPrompt.Response => win.openConfirmPrompt(payload.message);
+  ): Ipc.OpenConfirmPrompt.Response => openConfirmPrompt(win, payload.message);
 
   openDirDialog = (
     e: IpcMainInvokeEvent,
-    win: AppWindow,
+    win: BrowserWindow,
     payload: Ipc.OpenDirDialog.Payload
   ): Ipc.OpenDirDialog.Response =>
-    win.openDirDialog(payload.workingDirectory, payload.title);
+    openDirDialog(win, payload.workingDirectory, payload.title);
 
   static handleIfWindow = (
     channel: IpcChannel,
-    listener: (event: IpcMainInvokeEvent, win: AppWindow, payload: any) => any
+    listener: (
+      event: IpcMainInvokeEvent,
+      win: BrowserWindow,
+      payload: any
+    ) => any
   ) => {
     ipcMain.handle(channel, (event: IpcMainInvokeEvent, payload: any) => {
-      const win = BrowserWindow.fromWebContents(event.sender) as AppWindow;
+      const win = BrowserWindow.fromWebContents(event.sender);
       return win ? listener(event, win, payload) : undefined;
     });
   };
 }
 
-export const addIpcToMainHandlers = (settingsFileStore: SettingsFileStore) => {
-  const ipcHandler = new IpcToMainHandler(settingsFileStore);
+export const addIpcToMainHandlers = () => {
+  const ipcHandler = new IpcToMainHandler();
 
   ipcMain.handle(IpcChannel.APP_VERSION, ipcHandler.appVersion);
 
