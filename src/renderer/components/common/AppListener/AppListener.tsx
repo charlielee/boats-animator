5d88a62418e48c53133faf50ffcca732ba2cf7ff
Charlie Lee <dev@charlielee.uk>
2021-12-05 14:36:59 +0000

    Add route change logging

--- src/renderer/components/common/AppListener/AppListener.tsx
+++ src/renderer/components/common/AppListener/AppListener.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { RootState } from "../../../redux/store";
 import { handleOnCloseButtonClick } from "../../../services/appListener/AppListenerService";
+import * as rLogger from "../../../services/rLogger/rLogger";
 
 const AppListeners = (): JSX.Element => {
+  const location = useLocation();
   const userPreferences = useSelector(
     (state: RootState) => state.userPreferences
   );
@@ -13,6 +16,11 @@ const AppListeners = (): JSX.Element => {
       handleOnCloseButtonClick(userPreferences)
     );
   }, [userPreferences]);
+
+  useEffect(() => {
+    rLogger.info("appListener.routeChange", location.pathname);
+  }, [location]);
+
   return <></>;
 };
 
