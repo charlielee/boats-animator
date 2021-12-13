43767da218932fda752c175127bdcce893a5c78d
Charlie Lee <dev@charlielee.uk>
2021-12-13 14:16:05 +0000

    Remove unused things

--- src/renderer/components/animator/Animator/Animator.tsx
+++ src/renderer/components/animator/Animator/Animator.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { TrackType } from "../../../../common/Project";
 import Content from "../../common/Content/Content";
 import IconName from "../../common/Icon/IconName";
@@ -16,11 +15,6 @@ import StatusToolbar from "../StatusToolbar/StatusToolbar";
 import Timeline from "../Timeline/Timeline";
 
 const Animator = (): JSX.Element => {
-  const [range, setRange] = useState(10);
-  const [check, setCheck] = useState(false);
-  const [checkSwitch, setCheckSwitch] = useState(false);
-  const [numberAndRange, setNumberAndRange] = useState(2);
-
   return (
     <Page>
       <PageBody>
