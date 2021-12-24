0631cfdf95d9c9ece20b966ff52a9236e300825e
Charlie Lee <dev@charlielee.uk>
2021-12-24 11:09:09 +0000

    Start devtools helper

---
+++ src/main/services/devToolsExtensions/DevToolsExtensions.ts
@@ -0,0 +1,6 @@
+const REACT_DEV_TOOLS_ID = "fmkadmapgofadopljbjfkapdkoienihi";
+const REDUX_DEV_TOOLS_ID = "lmhkpmbekcpmknklioeibfkpmmfibljd";
+
+const possibleLocations = (id: string) => {};
+
+const findExtension = (id: string) => {};
