3337e959dc7329e8becf378e21af7774d572b13c
Charlie Lee <dev@charlielee.uk>
2022-02-26 22:20:12 +0000

    Show currect take

--- src/renderer/components/animator/StatusToolbar/StatusToolbar.tsx
+++ src/renderer/components/animator/StatusToolbar/StatusToolbar.tsx
@@ -1,19 +1,26 @@
+import { useSelector } from "react-redux";
 import { PageRoute } from "../../../../common/PageRoute";
+import { Take } from "../../../../common/Project";
+import { zeroPad } from "../../../../common/utils";
+import { RootState } from "../../../redux/store";
 import Button from "../../common/Button/Button";
 import { ButtonColor } from "../../common/Button/ButtonColor";
-import IconName from "../../common/Icon/IconName";
 import Toolbar from "../../common/Toolbar/Toolbar";
 import ToolbarItem, {
   ToolbarItemAlign,
 } from "../../common/ToolbarItem/ToolbarItem";
 
 const StatusToolbar = (): JSX.Element => {
+  const [take] = useSelector((state: RootState) => [state.project.take]);
+
+  const makeTakeTitle = (take: Take) =>
+    `Shot ${zeroPad(take.shotNumber, 3)} Take ${zeroPad(take.takeNumber, 2)}`;
+
   return (
     <Toolbar borderBottom>
       <ToolbarItem stretch align={ToolbarItemAlign.LEFT}>
         <Button
-          title="Shot 001 Take 01"
-          icon={IconName.CIRCLE_UP}
+          title={take ? makeTakeTitle(take) : "No take"}
           onClick={PageRoute.STARTUP_MODAL}
           color={ButtonColor.TRANSPARENT}
         />
