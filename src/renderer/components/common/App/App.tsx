a30fa173b5a01b73821e5ae35d5612a92a5195c7
Charlie Lee <dev@charlielee.uk>
2022-03-20 14:58:10 +0000

    Rename projectsModals dir to modals

--- src/renderer/components/common/App/App.tsx
+++ src/renderer/components/common/App/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from "react-router-dom";
 import { PageRoute } from "../../../../common/PageRoute";
 import Animator from "../../animator/Animator/Animator";
-import StartupModal from "../../projectsModals/StartupModal/StartupModal";
+import StartupModal from "../../modals/StartupModal/StartupModal";
 import AppListener from "../AppListener/AppListener";
 import AppLoad from "../AppLoad/AppLoad";
 
