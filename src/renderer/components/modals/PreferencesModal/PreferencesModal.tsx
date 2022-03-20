68f6c07d55c7fe3f6706d9fa4d17be946e23ce94
Charlie Lee <dev@charlielee.uk>
2022-03-20 18:10:14 +0000

    Display app version in settings

--- src/renderer/components/modals/PreferencesModal/PreferencesModal.tsx
+++ src/renderer/components/modals/PreferencesModal/PreferencesModal.tsx
@@ -1,16 +1,24 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { PageRoute } from "../../../../common/PageRoute";
 import { editUserPreferences } from "../../../redux/app/actions";
 import { RootState } from "../../../redux/store";
+import Button from "../../common/Button/Button";
 import Content from "../../common/Content/Content";
 import ContentBlock from "../../common/ContentBlock/ContentBlock";
+import IconName from "../../common/Icon/IconName";
 import InputGroup from "../../common/Input/InputGroup/InputGroup";
 import InputLabel from "../../common/Input/InputLabel/InputLabel";
 import InputSwitch from "../../common/Input/InputSwitch/InputSwitch";
 import Modal from "../../common/Modal/Modal";
 import ModalBody from "../../common/ModalBody/ModalBody";
+import ModalFooter from "../../common/ModalFooter/ModalFooter";
 import Page from "../../common/Page/Page";
 import PageBody from "../../common/PageBody/PageBody";
+import Toolbar from "../../common/Toolbar/Toolbar";
+import ToolbarItem, {
+  ToolbarItemAlign,
+} from "../../common/ToolbarItem/ToolbarItem";
 
 const PreferencesModal = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -18,6 +26,15 @@ const PreferencesModal = (): JSX.Element => {
     take: state.project.take,
     userPreferences: state.app.userPreferences,
   }));
+  const [appVersion, setAppVersion] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const version = await window.preload.ipcToMain.appVersion();
+      setAppVersion(version);
+    })();
+  }, []);
+
   return (
     <Modal onClose={take ? PageRoute.ANIMATOR : PageRoute.STARTUP_MODAL}>
       <ModalBody>
@@ -46,6 +63,21 @@ const PreferencesModal = (): JSX.Element => {
           </PageBody>
         </Page>
       </ModalBody>
+
+      <ModalFooter>
+        <Toolbar borderTop>
+          <ToolbarItem align={ToolbarItemAlign.LEFT}>
+            <Button
+              title="Open app data folder"
+              icon={IconName.FOLDER}
+              onClick={() => console.log("todo")}
+            />
+          </ToolbarItem>
+          <ToolbarItem align={ToolbarItemAlign.RIGHT}>
+            Version {appVersion}
+          </ToolbarItem>
+        </Toolbar>
+      </ModalFooter>
     </Modal>
   );
 };
