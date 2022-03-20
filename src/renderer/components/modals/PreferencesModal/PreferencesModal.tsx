7cbb7254478a2d716055a6f3a585afe061069eb6
Charlie Lee <dev@charlielee.uk>
2022-03-20 17:59:24 +0000

    Add playCaptureSound switch to preferences

--- src/renderer/components/modals/PreferencesModal/PreferencesModal.tsx
+++ src/renderer/components/modals/PreferencesModal/PreferencesModal.tsx
@@ -1,22 +1,47 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { PageRoute } from "../../../../common/PageRoute";
+import { editUserPreferences } from "../../../redux/app/actions";
 import { RootState } from "../../../redux/store";
 import Content from "../../common/Content/Content";
 import ContentBlock from "../../common/ContentBlock/ContentBlock";
+import InputGroup from "../../common/Input/InputGroup/InputGroup";
+import InputLabel from "../../common/Input/InputLabel/InputLabel";
+import InputSwitch from "../../common/Input/InputSwitch/InputSwitch";
 import Modal from "../../common/Modal/Modal";
 import ModalBody from "../../common/ModalBody/ModalBody";
 import Page from "../../common/Page/Page";
 import PageBody from "../../common/PageBody/PageBody";
 
 const PreferencesModal = (): JSX.Element => {
-  const take = useSelector((state: RootState) => state.project.take);
+  const dispatch = useDispatch();
+  const { take, userPreferences } = useSelector((state: RootState) => ({
+    take: state.project.take,
+    userPreferences: state.app.userPreferences,
+  }));
   return (
     <Modal onClose={take ? PageRoute.ANIMATOR : PageRoute.STARTUP_MODAL}>
       <ModalBody>
         <Page>
           <PageBody>
             <Content>
-              <ContentBlock title="Preferences">Hi</ContentBlock>
+              <ContentBlock title="Preferences">
+                <InputGroup row>
+                  <InputSwitch
+                    id="preferencesPlayCaptureSound"
+                    checked={userPreferences.playCaptureSound}
+                    onChange={() =>
+                      dispatch(
+                        editUserPreferences({
+                          playCaptureSound: !userPreferences.playCaptureSound,
+                        })
+                      )
+                    }
+                  />
+                  <InputLabel inputId="preferencesPlayCaptureSound">
+                    Play capture sound
+                  </InputLabel>
+                </InputGroup>
+              </ContentBlock>
             </Content>
           </PageBody>
         </Page>
