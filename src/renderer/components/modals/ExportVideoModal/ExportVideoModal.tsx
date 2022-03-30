780184433988e8d425284a053ca3e4cfd3090caf
Charlie Lee <dev@charlielee.uk>
2022-03-30 22:44:42 +0100

    Add Start Export button

--- src/renderer/components/modals/ExportVideoModal/ExportVideoModal.tsx
+++ src/renderer/components/modals/ExportVideoModal/ExportVideoModal.tsx
@@ -1,13 +1,21 @@
 import { PageRoute } from "../../../../common/PageRoute";
+import Button from "../../common/Button/Button";
+import { ButtonColor } from "../../common/Button/ButtonColor";
 import Content from "../../common/Content/Content";
 import ContentBlock from "../../common/ContentBlock/ContentBlock";
+import IconName from "../../common/Icon/IconName";
 import InputGroup from "../../common/Input/InputGroup/InputGroup";
 import InputLabel from "../../common/Input/InputLabel/InputLabel";
 import InputNumber from "../../common/Input/InputNumber/InputNumber";
 import Modal from "../../common/Modal/Modal";
 import ModalBody from "../../common/ModalBody/ModalBody";
+import ModalFooter from "../../common/ModalFooter/ModalFooter";
 import Page from "../../common/Page/Page";
 import PageBody from "../../common/PageBody/PageBody";
+import Toolbar from "../../common/Toolbar/Toolbar";
+import ToolbarItem, {
+  ToolbarItemAlign,
+} from "../../common/ToolbarItem/ToolbarItem";
 
 const ExportVideoModal = (): JSX.Element => {
   return (
@@ -19,7 +27,7 @@ const ExportVideoModal = (): JSX.Element => {
               <ContentBlock title="Export Video">
                 <InputGroup>
                   <InputLabel inputId="exportVideoLocation">
-                    Export location
+                    Export Location
                   </InputLabel>
                   <InputNumber
                     id="exportVideoLocation"
@@ -31,10 +39,22 @@ const ExportVideoModal = (): JSX.Element => {
 
                 <InputGroup>
                   <InputLabel inputId="exportVideoQualityPreset">
-                    FFmpeg quality preset
+                    Quality Preset
                   </InputLabel>
                   <InputNumber
                     id="exportVideoQualityPreset"
+                    min={0}
+                    max={1}
+                    onChange={(n) => console.log(n)}
+                  />
+                </InputGroup>
+
+                <InputGroup>
+                  <InputLabel inputId="exportVideoFFmpegArguments">
+                    FFmpeg Arguments (advanced)
+                  </InputLabel>
+                  <InputNumber
+                    id="exportVideoFFmpegArguments"
                     min={0}
                     max={1}
                     onChange={(n) => console.log(n)}
@@ -45,6 +65,19 @@ const ExportVideoModal = (): JSX.Element => {
           </PageBody>
         </Page>
       </ModalBody>
+
+      <ModalFooter>
+        <Toolbar borderTop>
+          <ToolbarItem align={ToolbarItemAlign.LEFT}>
+            <Button
+              title="Start Export"
+              color={ButtonColor.PRIMARY}
+              icon={IconName.VIDEO}
+              onClick={() => console.log("todo")}
+            />
+          </ToolbarItem>
+        </Toolbar>
+      </ModalFooter>
     </Modal>
   );
 };
