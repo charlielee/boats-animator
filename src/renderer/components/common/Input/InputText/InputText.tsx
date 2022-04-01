4f3e5eb82c085ed6cddd8f4e8ea09ab05e74def3
Charlie Lee <dev@charlielee.uk>
2022-04-01 19:02:13 +0100

    Add InputText component

---
+++ src/renderer/components/common/Input/InputText/InputText.tsx
@@ -0,0 +1,14 @@
+interface InputTextProps {
+  id?: string;
+  value: string;
+  onChange(newValue: string): void;
+}
+
+const InputText = ({ id, value, onChange }: InputTextProps): JSX.Element => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    onChange(event.target.value);
+
+  return <input id={id} type="text" onChange={handleChange} value={value} />;
+};
+
+export default InputText;
