ac2ac3f50e89ec8a0d89c9fb02e2f775d335d89e
Charlie Lee <dev@charlielee.uk>
2022-08-07 09:20:01 +0100

    Tidy up usePlayback

--- src/renderer/hooks/usePlayback.tsx
+++ src/renderer/hooks/usePlayback.tsx
@@ -1,10 +1,12 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FrameRate, TimelineIndex } from "../../common/Flavors";
 import * as rLogger from "../services/rLogger/rLogger";
 import useRequestAnimationFrame from "./useRequestAnimationFrame";
 
 interface UsePlaybackOptions {
+  // The zero based index of the first frame to play
   startTimelineIndex: TimelineIndex;
+  // The zero based index of the last frame to play
   stopTimelineIndex: TimelineIndex | undefined;
   frameRate: FrameRate;
 }
@@ -20,53 +22,46 @@ const usePlayback = ({
   );
 
   const delay = 1000 / frameRate;
-
   const previousTime = useRef<number>(0);
-  // const animationFrameNumber = useRef<number>(0);
   const animationFrameIndex = useRef<number | undefined>(undefined);
 
   const [start, stop] = useRequestAnimationFrame((newTime) => {
-    console.log("animate", newTime);
+    console.log("animate", animationFrameIndex.current);
 
     if (animationFrameIndex.current === undefined) {
       throw "Must set animationFrameIndex.current before running";
     }
 
+    // TODO TODO TODO need to handle the first frame
+
+    // if (animationFrameIndex.current === 0) {
+    //   previousTime.current = newTime;
+    //   return;
+    // }
+
     if (newTime >= previousTime.current + delay) {
-      previousTime.current = newTime;
-      // animationFrameIndex.current += 1;
       console.log("frame", animationFrameIndex.current);
+      previousTime.current = newTime;
       updateFrameIndex(animationFrameIndex.current + 1);
-      // setTimelineIndex(animationFrameIndex.current);
-      // animationFrameNumber.current += 1;
     }
 
     if (stopTimelineIndex && animationFrameIndex.current > stopTimelineIndex) {
-      console.log("stopped");
-      updateFrameIndex(undefined);
-      stop();
+      stopPlayback();
     }
   });
 
   const startPlayback = () => {
+    logPlayback("usePlayback.startPlayback");
     if (stopTimelineIndex) {
-      // animationFrameIndex.current = startTimelineIndex;
       updateFrameIndex(0);
       start();
-      // timelineIndex
-      // setTimelineIndex(0);
     }
-    logPlayback("usePlayback.startPlayback");
   };
 
   const stopPlayback = () => {
-    stop();
-    // animationFrameIndex.current = stopTimelineIndex;
-    updateFrameIndex(undefined);
-
-    // animationFrameNumber.current = 0;
-    // setTimelineIndex(undefined);
     logPlayback("usePlayback.stopPlayback");
+    stop();
+    updateFrameIndex(undefined);
   };
 
   const updateFrameIndex = (i: number | undefined) => {
@@ -82,15 +77,9 @@ const usePlayback = ({
       timelineIndex: animationFrameIndex.current ?? "(showing live view)",
     });
 
-  // useEffect(() => {
-  //   console.log("timelineIndex", timelineIndex);
-  //   if (
-  //     timelineIndex.current === undefined ||
-  //     (stopTimelineIndex && timelineIndex.current >= stopTimelineIndex)
-  //   ) {
-  //     stopPlayback();
-  //   }
-  // }, [timelineIndex.current]);
+  useEffect(() => {
+    console.log("timelineIndex", timelineIndex);
+  }, [timelineIndex]);
 
   return [startPlayback, stopPlayback, timelineIndex];
 };
