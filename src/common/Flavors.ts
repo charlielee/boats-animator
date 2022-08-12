4ae5062c5ba2e88857ec5db28b8e50f060cd5e6d
Charlie Lee <dev@charlielee.uk>
2022-08-12 21:52:57 +0100

    tidy up

--- src/common/Flavors.ts
+++ src/common/Flavors.ts
@@ -6,7 +6,6 @@ type Flavor<T, FlavorT> = T & Flavoring<FlavorT>;
 export type Primitive = string | number | boolean | NumberFlavor | StringFlavor;
 
 export type FrameCount = Flavor<number, "FrameCount">;
-// export type FrameNumber = Flavor<number, "FrameNumber">;
 export type FrameRate = Flavor<number, "FrameRate">;
 export type TimelineIndex = Flavor<number, "TimelineIndex">;
 type NumberFlavor = FrameCount | FrameRate | TimelineIndex;
