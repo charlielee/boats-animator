7f37c498106ef71ba34ed9c67e3c7b6b82708662
Charlie Lee <dev@charlielee.uk>
2022-08-21 12:34:13 +0100

    Test button onclick when disabled

--- src/renderer/components/common/Button/Button.test.tsx
+++ src/renderer/components/common/Button/Button.test.tsx
@@ -3,6 +3,8 @@ import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from "../../../testing/TestHelper";
 import Button from "./Button";
 
+const getButton = () => screen.getByRole("button");
+
 it("should call onClick when button is clicked", async () => {
   const buttonTitle = "Hello";
   const onClick = jest.fn();
@@ -10,7 +12,20 @@ it("should call onClick when button is clicked", async () => {
 
   expect(onClick).not.toBeCalled();
 
-  await userEvent.click(screen.getByText(buttonTitle));
+  await userEvent.click(getButton());
 
   expect(onClick).toBeCalled();
+});
+
+it("should not call onClick when button is disabled", async () => {
+  const buttonTitle = "Hello";
+  const onClick = jest.fn();
+  renderWithProviders(
+    <Button title={buttonTitle} onClick={onClick} disabled />
+  );
+
+  await userEvent.click(getButton());
+
+  expect(onClick).not.toBeCalled();
+  expect(getButton()).toBeDisabled();
 });
