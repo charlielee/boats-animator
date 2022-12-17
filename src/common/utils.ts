644046f5c330845acf325c5feb955683bc6e2aaf
Charlie Lee <dev@charlielee.uk>
2022-12-17 14:51:38 +0000

    Tidy up

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -21,4 +21,11 @@ export const stringToArray = (items: string) =>
     .match(/[^\s"']+|"([^"]*)"/gim)
     ?.map((arg: string) => arg.replace(/"|'/g, ""));
 
-export const playbackSpeeds = [0.1, 0.25, 0.5, 1, 2, 4];
+export const PLAYBACK_SPEEDS = {
+  "0.1×": 0.1,
+  "0.25×": 0.25,
+  "0.5×": 0.5,
+  "1.0×": 1,
+  "2.0×": 2,
+  "4.0×": 4,
+};
--- src/renderer/components/animator/PlaybackSpeedSelect/PlaybackSpeedSelect.tsx
+++ src/renderer/components/animator/PlaybackSpeedSelect/PlaybackSpeedSelect.tsx
@@ -1,13 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
-import { playbackSpeeds } from "../../../../common/utils";
+import { PLAYBACK_SPEEDS } from "../../../../common/utils";
 import { setPlaybackSpeed } from "../../../redux/project/actions";
 import { RootState } from "../../../redux/store";
 import InputSelect from "../../common/Input/InputSelect/InputSelect";
-
-const playbackSpeedOptions = playbackSpeeds.reduce(
-  (output, s) => ({ ...output, [`${s}x`]: s }),
-  {} as Record<string, number>
-);
 
 const PlaybackSpeedSelect = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -17,7 +12,7 @@ const PlaybackSpeedSelect = (): JSX.Element => {
 
   return (
     <InputSelect
-      options={playbackSpeedOptions}
+      options={PLAYBACK_SPEEDS}
       value={playbackSpeed}
       onChange={(newValue) => dispatch(setPlaybackSpeed(parseFloat(newValue)))}
       title={`Playback Speed ${playbackSpeed}x`}
