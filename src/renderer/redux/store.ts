2fd66be8388a3a7bbb3d26ff0fd8e536406c08d4
Charlie Lee <dev@charlielee.uk>
2022-12-18 17:32:25 +0000

    Reconnect capture middleware

--- src/renderer/redux/store.ts
+++ src/renderer/redux/store.ts
@@ -1,16 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { createCaptureMiddleware } from "./capture/middleware";
 import { appReducer } from "./slices/appSlice";
 import { projectReducer } from "./slices/projectSlice";
 
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(thunk, createCaptureMiddleware))
-// );
-
-export const store = configureStore({
-  reducer: { app: appReducer, project: projectReducer },
+const rootReducer = combineReducers({
+  app: appReducer,
+  project: projectReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["imagingDevice/ATTACH_STREAM_TO_VIDEO"],
+      },
+    }).concat(createCaptureMiddleware),
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
