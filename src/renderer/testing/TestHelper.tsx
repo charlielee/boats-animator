bffc7d81e67b262298cbe884b8f1bff95573d6db
Charlie Lee <dev@charlielee.uk>
2024-03-29 12:41:18 +0000

    Add redux to TestHelper

--- src/renderer/testing/TestHelper.tsx
+++ src/renderer/testing/TestHelper.tsx
@@ -1,13 +1,19 @@
 import { render as rtlRender } from "@testing-library/react";
 import { ReactNode } from "react";
+import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
+import { store } from "../redux/store";
 
 interface TestAppProps {
   children: ReactNode;
 }
 
 const TestApp = ({ children }: TestAppProps) => {
-  return <MemoryRouter>{children}</MemoryRouter>;
+  return (
+    <Provider store={store}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </Provider>
+  );
 };
 
 export const renderWithProviders = (ui: any, options?: any) =>
