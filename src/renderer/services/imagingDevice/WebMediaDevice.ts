b5aa3e1ae3db0043bf6e23663a13b8ae3aaa13ac
Charlie Lee <dev@charlielee.uk>
2024-11-21 22:31:09 +0800

    more logs

--- src/renderer/services/imagingDevice/WebMediaDevice.ts
+++ src/renderer/services/imagingDevice/WebMediaDevice.ts
@@ -161,6 +161,9 @@ class WebMediaDevice implements ImagingDevice {
     const { width, height } = this.stream.getVideoTracks()[0].getSettings();
     console.log("settings", this.stream.getVideoTracks()[0].getSettings());
     console.log("constraints", this.stream.getVideoTracks()[0].getConstraints());
+    console.log("photoSettings", this.imageCapture?.getPhotoSettings());
+    console.log("photoCapabilities", this.imageCapture?.getPhotoCapabilities());
+    console.log("videotrack", this.stream.getVideoTracks()[0]);
     if (width === undefined || height === undefined) {
       throw "Unable to device getResolution";
     }
