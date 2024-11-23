47d43dcfe260b8f45b6d3750e94f59c57deb86e9
Charlie Lee <dev@charlielee.uk>
2024-11-23 12:45:19 +0800

    Flex switches in preferences

--- src/renderer/components/ui/UiSwitch/UiSwitch.tsx
+++ src/renderer/components/ui/UiSwitch/UiSwitch.tsx
@@ -1,4 +1,4 @@
-import { Flex, Switch, SwitchProps } from "@mantine/core";
+import { Flex, FlexProps, Switch, SwitchProps } from "@mantine/core";
 
 interface UiSwitchProps {
   label?: string;
@@ -8,13 +8,14 @@ interface UiSwitchProps {
 }
 
 export const UiSwitch = ({ label, checked, inList = false, onChange }: UiSwitchProps) => {
+  const inListFlexProps: FlexProps = inList ? { justify: "flex-end" } : {};
   const inListProps: SwitchProps = inList ? { size: "xs" } : {};
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     onChange(event.currentTarget.checked);
 
   return (
-    <Flex justify="flex-end">
+    <Flex {...inListFlexProps}>
       <Switch label={label} checked={checked} onChange={handleChange} {...inListProps} />
     </Flex>
   );
