c865a6165648a964ed9d062ef1db5e1f4769d167
Charlie Lee <dev@charlielee.uk>
2024-11-25 20:31:22 +0800

    Remove old unused loader

--- src/renderer/redux/thunks.ts
+++ src/renderer/redux/thunks.ts
@@ -1,5 +1,5 @@
 import { Action, ThunkDispatch } from "@reduxjs/toolkit";
-import { editUserPreferences, startLoading, stopLoading } from "./slices/appSlice";
+import { editUserPreferences } from "./slices/appSlice";
 import { RootState } from "./store";
 
 export const loadSavedPreferences = () => {
@@ -7,19 +7,6 @@ export const loadSavedPreferences = () => {
     return (async () => {
       const savedPreferences = await window.preload.ipcToMain.getUserPreferences();
       dispatch(editUserPreferences(savedPreferences));
-    })();
-  };
-};
-
-export const withLoader = (loadingMessage: string, callback: () => Promise<void>) => {
-  return (dispatch: ThunkDispatch<RootState, void, Action>) => {
-    return (async () => {
-      try {
-        dispatch(startLoading(loadingMessage));
-        await callback();
-      } finally {
-        dispatch(stopLoading());
-      }
     })();
   };
 };
