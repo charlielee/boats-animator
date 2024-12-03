f55063fd7274abc1be7d163f4f0440e07c514aac
Charlie Lee <dev@charlielee.uk>
2024-12-03 17:06:07 +0800

    Fix timeline scroll

--- src/renderer/components/animator/Animator/Animator.tsx
+++ src/renderer/components/animator/Animator/Animator.tsx
@@ -9,6 +9,7 @@ import { Preview } from "../Preview/Preview";
 import { PreviewToolbar } from "../PreviewToolbar/PreviewToolbar";
 import { Timeline } from "../Timeline/Timeline";
 import TitleToolbar from "../TitleToolbar/TitleToolbar";
+import "./Animator.css";
 
 export const Animator = (): JSX.Element => {
   return (
@@ -19,14 +20,9 @@ export const Animator = (): JSX.Element => {
           <Preview />
           <PreviewToolbar />
 
-          <Group
-            gap="xs"
-            align="stretch"
-            wrap="nowrap"
-            style={{ overflow: "hidden", maxWidth: "100%" }}
-          >
+          <Group className="animator__overlay-tabs-pane-and-timeline" gap="xs">
             <OverlayTabsPane />
-            <Stack flex={1} gap={0}>
+            <Stack flex={1} gap={0} className="animator__frame-toolbar-and-timeline">
               <FrameToolbar />
               <Timeline />
             </Stack>
