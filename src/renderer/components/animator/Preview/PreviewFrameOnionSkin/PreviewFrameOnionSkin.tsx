c7c48a4d775234010cebb4e511c314c474a376dd
Charlie Lee <dev@charlielee.uk>
2024-12-05 15:10:20 +0800

    start handling multiple onion skin frames

--- src/renderer/components/animator/Preview/PreviewFrameOnionSkin/PreviewFrameOnionSkin.tsx
+++ src/renderer/components/animator/Preview/PreviewFrameOnionSkin/PreviewFrameOnionSkin.tsx
@@ -1,12 +1,11 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../redux/store";
-import { getLastTrackItem } from "../../../../services/project/projectCalculator";
-import PreviewFrame from "../PreviewFrame/PreviewFrame";
 import { useContext } from "react";
-import PlaybackContext from "../../../../context/PlaybackContext/PlaybackContext";
-import useProjectAndTake from "../../../../hooks/useProjectAndTake";
-import { ProjectFilesContext } from "../../../../context/ProjectFilesContext.tsx/ProjectFilesContext";
+import { useSelector } from "react-redux";
 import { TrackItem } from "../../../../../common/project/TrackItem";
+import PlaybackContext from "../../../../context/PlaybackContext/PlaybackContext";
+import { ProjectFilesContext } from "../../../../context/ProjectFilesContext.tsx/ProjectFilesContext";
+import useProjectAndTake from "../../../../hooks/useProjectAndTake";
+import { RootState } from "../../../../redux/store";
+import PreviewFrame from "../PreviewFrame/PreviewFrame";
 
 export const PreviewFrameOnionSkin = () => {
   const { take } = useProjectAndTake();
@@ -19,12 +18,23 @@ export const PreviewFrameOnionSkin = () => {
   const enableOnionSkin = useSelector((state: RootState) => state.project.enableOnionSkin);
   const onionSkinOpacity = useSelector((state: RootState) => state.project.onionSkinOpacity);
 
-  const onionSkinTrackItem = getLastTrackItem(take.frameTrack);
-  const onionSkinSrc = onionSkinTrackItem ? getTrackItemObjectURL(onionSkinTrackItem) : undefined;
-
-  const showOnionSkinFrame = liveViewVisible && onionSkinSrc !== undefined && enableOnionSkin;
-
-  return (
-    <PreviewFrame src={onionSkinSrc} opacity={showOnionSkinFrame ? onionSkinOpacity / 100 : 0} />
+  const onionSkinFramesVisible = useSelector(
+    (state: RootState) => state.project.onionSkinFramesVisible
   );
+  const trackItems = take.frameTrack.trackItems.slice(onionSkinFramesVisible * -1);
+
+  const showOnionSkinFrames = liveViewVisible && enableOnionSkin;
+
+  // todo handle not showing onion skin when rapid capture (probably due to getTrackItemFileInfo being undefined)
+  console.log(trackItems);
+
+  if (showOnionSkinFrames) {
+    return trackItems.map((trackItem) => (
+      <PreviewFrame
+        src={getTrackItemObjectURL(trackItem)}
+        opacity={onionSkinOpacity / 100}
+        key={trackItem.id}
+      />
+    ));
+  }
 };
