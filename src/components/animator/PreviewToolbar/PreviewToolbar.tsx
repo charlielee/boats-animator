74de2222b30abe42a463a8c1625c8f469421fd37
Charlie Lee <dev@charlielee.uk>
2025-07-20 16:57:23 +0800

    Add useCaptureContext hook

--- src/components/animator/PreviewToolbar/PreviewToolbar.tsx
+++ src/components/animator/PreviewToolbar/PreviewToolbar.tsx
@@ -1,12 +1,12 @@
 import { Group } from "@mantine/core";
 import { useContext } from "react";
-import CaptureContext from "../../../context/CaptureContext/CaptureContext";
+import { useCaptureContext } from "../../../context/CaptureContext/CaptureContext";
 import PlaybackContext from "../../../context/PlaybackContext/PlaybackContext";
 import IconName from "../../common/Icon/IconName";
 import { UiActionIcon, UiActionIconRole } from "../../ui/UiActionIcon/UiActionIcon";
 
 export const PreviewToolbar = () => {
-  const { captureImage } = useContext(CaptureContext);
+  const { captureImage } = useCaptureContext();
   const { stopPlayback, liveViewVisible } = useContext(PlaybackContext);
 
   const handleClickCaptureButton = () => {
--- src/context/CaptureContext/CaptureContext.tsx
+++ src/context/CaptureContext/CaptureContext.tsx
@@ -1,13 +1,17 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 interface CaptureContextProps {
   captureImage: () => void;
 }
 
-const defaultValue: CaptureContextProps = {
-  captureImage: () => undefined,
+export const CaptureContext = createContext<CaptureContextProps | undefined>(undefined);
+
+export const useCaptureContext = () => {
+  const context = useContext(CaptureContext);
+
+  if (context === undefined) {
+    throw new Error("Must be called within CaptureContextProvider");
+  }
+
+  return context;
 };
-
-const CaptureContext = createContext<CaptureContextProps>(defaultValue);
-
-export default CaptureContext;
